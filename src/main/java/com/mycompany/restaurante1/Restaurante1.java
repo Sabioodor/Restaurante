fbb503b9b0655b6cba535b88e00d808a51241240
Sabioodor <rafael.ponciano.pereira@gmail.com>
2024-08-15 14:40:57 -0300

    Subindo arquivos do projeto resta

---
+++ src/main/java/com/mycompany/restaurante1/Restaurante1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurante1;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Restaurante1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
